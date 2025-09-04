b09ff153e9769391f9591a906ff040e1712c4327
Trinidad Perea <trinidadperea10@gmail.com>
2025-09-04 16:57:22 -0300

    Agrego tinder app

---
+++ practicos/tp5/tinderApp/src/main/java/edu/egg/tinder/entities/Foto.java
@@ -0,0 +1,36 @@
+package edu.egg.tinder.entities;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "foto")
+public class Foto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String mime;
+	private String nombre;
+	
+	//indica que el tipo de dato sera pesado
+	@Lob
+	@Basic(fetch = FetchType.LAZY) // solo carga el contenido cuando se le pide
+	private byte[] contenido; 
+
+}
---
+++ practicos/tp5/tinderApp/src/main/java/edu/egg/tinder/entities/Usuario.java
@@ -0,0 +1,50 @@
+package edu.egg.tinder.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String mail;
+	private String clave;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date alta;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date baja;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_zona")
+	private Zona zona;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Mascota> mascotas;
+	
+	@OneToOne
+	private Foto foto;
+}
