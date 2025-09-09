d8b8c8892489a0c7d6190e6f4ac2ce9b6dccb561
Perla <valerioperla98@gmail.com>
2025-09-09 16:31:35 -0300

    ejercicio 6b

---
+++ practicos/tp6 - testing/ej6b/TestConJUnit/src/testconjunit/services/RectanguloService.java
@@ -0,0 +1,22 @@
+package testconjunit.services;
+
+import java.awt.geom.Area;
+
+import testconjunit.entities.Rectangulo;
+
+public class RectanguloService {
+
+	public RectanguloService() {
+		
+	}
+	
+	public double calcularArea(Rectangulo r) {
+		return r.getAncho() * r.getLargo();
+		
+	}
+	
+	public double calcularPerimetro(Rectangulo r) {
+		return  2 * (r.getAncho() + r.getLargo());
+		
+	}
+}
---
+++ practicos/tp6 - testing/ej6b/jUnit2021/src/main/java/com/tpa/jUnit/JUnit2021Application.java
@@ -0,0 +1,13 @@
+package com.tpa.jUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit2021Application.class, args);
+	}
+
+}
