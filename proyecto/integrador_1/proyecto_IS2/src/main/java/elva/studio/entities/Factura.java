c87d0b9ae341cdad22499b61b3d6658631e1713e
Trinidad Perea <trinidadperea10@gmail.com>
2025-09-16 18:51:55 -0300

    Agrego parte del front

--- proyecto/integrador_1/proyecto_IS2/src/main/java/elva/studio/entities/Factura.java
+++ proyecto/integrador_1/proyecto_IS2/src/main/java/elva/studio/entities/Factura.java
@@ -1,14 +1,12 @@
 package elva.studio.entities;
 
 import java.io.Serializable;
-//import java.time.LocalDateTime;
 import java.util.Date;
 
 import elva.studio.enumeration.EstadoCuota;
+import elva.studio.enumeration.EstadoFactura;
 import elva.studio.enumeration.Mes;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,34 +20,28 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="cuota_mensual")
+@Table(name="factura")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CuotaMensual implements Serializable{
-	
+public class Factura implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Enumerated(EnumType.STRING)
-	private Mes mes;
-	
-	private Long anio;
-	
-	@Enumerated(EnumType.STRING)
-	private EstadoCuota estado;
+	private Long numeroFactura;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date fechaVencimiento;
+	private Date fechaFactura;
+	
+	private double totalPagado;
+	
+	private EstadoFactura estado;
 	
 	private boolean eliminado;
 	
 	@ManyToOne
-	@JoinColumn(name="fk_valor_cuota")
-	private ValorCuota valorCuota;
+	@JoinColumn(name="fk_formaDePago")
+	private FormaDePago formaDePago;
 	
-	@ManyToOne
-	@JoinColumn(name="fk_socio")
-	private Socio socio;
 }
