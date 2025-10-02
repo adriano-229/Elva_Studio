2d2a42765987cc0e55321fff8993eac50d561f58
adriano-229 <adrianosantinof@gmail.com>
2025-10-02 10:15:31 -0300

    restarting

--- practicos/parte2/tp1 - repaso scrum y mvc/src/main/java/com/elva/tp1/entity/Activable.java
+++
@@ -1,7 +0,0 @@
-package com.elva.tp1.entity;
-
-public interface Activable {
-    boolean isActivo();
-
-    void desactivar();
-}
--- practicos/parte2/tp1 - repaso scrum y mvc/src/main/java/com/elva/tp1/repository/ProvinciaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elva.tp1.repository;
-
-import com.elva.tp1.entity.Provincia;
-
-public interface ProvinciaRepository extends ActivableRepository<Provincia, Integer> {
-    // Puede agregarse un método ordenado si se requiere: List<Provincia> findByActivoTrueOrderByNombreAsc();
-}
-
