b38ce48f7e331aa9bc85004f4ac5b1fad6eb5dde
adriano-229 <adrianosantinof@gmail.com>
2025-10-03 13:14:15 -0300

    Cleaned repository: just needed methods and Optional for all.

--- practicos/parte2/tp1/src/main/java/com/elva/tp1/repository/ProveedorRepository.java
+++ practicos/parte2/tp1/src/main/java/com/elva/tp1/repository/ProveedorRepository.java
@@ -4,7 +4,11 @@ import com.elva.tp1.domain.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+    Optional<Proveedor> findByCuit(String cuit);
+
 }
 
--- practicos/parte2/tp1/src/main/java/com/elva/tp1/repository/ProvinciaRepository.java
+++ practicos/parte2/tp1/src/main/java/com/elva/tp1/repository/ProvinciaRepository.java
@@ -1,12 +1,19 @@
 package com.elva.tp1.repository;
 
+import com.elva.tp1.domain.Pais;
 import com.elva.tp1.domain.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
+import java.util.Optional;
 
 @Repository
 public interface ProvinciaRepository extends JpaRepository<Provincia, Long> {
-    List<Provincia> findByPaisId(Long paisId);
-    List<Provincia> findByActivoTrue();
+
+    Optional<Pais> findByNombreOrderByAsc(String nombreProvincia);
+
+
+    Optional<Pais> findByNombrePaisOrderByAsc(String nombrePais);
+
+
 }
