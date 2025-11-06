b7e9691483fc8db2fc68990496011a1aab4426f4
adriano-229 <adrianosantinof@gmail.com>
2025-11-06 11:41:09 -0300

    Implement Alquiler and DetalleFactura entities with relationships; add MercadoPagoController for payment processing

--- integrador_2/My Car/mycar/src/main/java/com/example/mycar/entities/Alquiler.java
+++ integrador_2/My Car/mycar/src/main/java/com/example/mycar/entities/Alquiler.java
@@ -1,35 +1,36 @@
 package com.example.mycar.entities;
 
-import java.util.Date;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "alquiler")
-public class Alquiler extends Base{
-	
-	@Column(name = "fecha_desde", nullable = false)
-	private Date fechaDesde;
-	
-	@Column(name = "fecha_hasta", nullable = false)
-	private Date fechaHasta;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
+public class Alquiler extends Base {
+
+    @Column(name = "fecha_desde", nullable = false)
+    private Date fechaDesde;
+
+    @Column(name = "fecha_hasta", nullable = false)
+    private Date fechaHasta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "documentacion_id")
-	private Documentacion documentacion;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
+    private Documentacion documentacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vehiculo_id")
-	private Vehiculo vehiculo;
+    private Vehiculo vehiculo;
+
+    @OneToOne
+    @JoinColumn(name = "detalle_factura_id")
+    private DetalleFactura detalleFactura;
 }
